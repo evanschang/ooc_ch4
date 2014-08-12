d0d99026e5251406f286a99b5ce358d1af551261
EvansChangNB <EvansChangNB@10.10.1.101>
2014-08-12 19:00:57 +0800

    ooc ch4 halfway

---
+++ OOCch4/Circle.r
@@ -0,0 +1,10 @@
+#ifndef CIRCLE_R_
+#define CIRCLE_R_
+
+#include "Point.r"
+
+struct Circle {
+	const struct Point _; int rad;
+};
+
+#endif /* CIRCLE_R_ */
---
+++ OOCch4/new.h
@@ -0,0 +1,15 @@
+/*
+ * new.h
+ *
+ *  Created on: 2014¦~8¤ë12¤é
+ *      Author: EvansChang
+ */
+
+#ifndef NEW_H_
+#define NEW_H_
+
+void* new (const void *Class, ...);
+void delete (void *item);
+void draw (const void *self);
+
+#endif /* NEW_H_ */
